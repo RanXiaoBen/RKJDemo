8ae00233d39d8db08b67d1336a1a13c371ee60e8
Allianture <15523960562@163.com>
2017-07-16 15:30:49 +0800

    提交

---
+++ RKJDemo/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  DianMing
+//
+//  Created by Allianture on 2017/7/16.
+//  Copyright © 2017年 kitty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface model : NSObject
+
+@end
---
+++ RKJDemo/model.m
@@ -0,0 +1,13 @@
+//
+//  model.m
+//  DianMing
+//
+//  Created by Allianture on 2017/7/16.
+//  Copyright © 2017年 kitty. All rights reserved.
+//
+
+#import "model.h"
+
+@implementation model
+
+@end
